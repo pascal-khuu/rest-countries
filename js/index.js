f5eafa258a96d9151afd40c65adbef13f03213c1
Pascal Khuu <pascal.khuu@orange.fr>
2022-01-11 17:25:00 +0100

    script  js: list of countries of EU

---
+++ js/index.js
@@ -0,0 +1,14 @@
+async function render() {
+    const response = await fetch('https://restcountries.com/v3.1/region/europe?fields=name');
+    countries = await response.json();
+    let list = '';
+    for (const country of countries) {
+        list = list + '<li>' + country.name.official + '</li>'
+    }
+
+    const target = document.getElementById("countries");
+    target.innerHTML = list;
+
+}
+
+addEventListener('load', render());
