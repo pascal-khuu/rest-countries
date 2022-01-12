0c9bed9a4b95e7deedab0792400afb8c81ee8088
Pascal Khuu <pascal.khuu@orange.fr>
2022-01-12 15:07:23 +0100

    format number (area population), position right (content area population), title and header updated

--- js/table.js
+++ js/table.js
@@ -2,11 +2,12 @@ async function render() {
     const response = await fetch('https://restcountries.com/v3.1/region/europe?fields=name,capital,population,area');
     const countries = await response.json();
     let table = '';
+    const formatNumber = new Intl.NumberFormat('en-US');
     for (const country of countries) {
         table = table + `<tr>  
          <th>  ${ country.name.official }  </th>
-         <td>  ${ country.area }  </td>
-         <td>  ${ country.population }  </td>
+         <td class="text-end">  ${ formatNumber.format(country.area) }  </td>
+         <td class="text-end">  ${ formatNumber.format(country.population) }  </td>
          <td>  ${ country.capital }  </td>
                 </tr>`;
     }
