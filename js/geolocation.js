37432cd0e2a991cdc5edce7c55ed4a8cdbc02bd6
Pascal Khuu <pascal.khuu@orange.fr>
2022-01-18 14:55:49 +0100

    maps of EU countries

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,5 +1,5 @@
 async function render() {
-    const response = await fetch('https://restcountries.com/v3.1/region/europe?fields=name,cca2,latlng');
+    const response = await fetch('https://restcountries.com/v3.1/region/europe?fields=name,cca2');
     const mapcountries = await response.json();
     let option = '';
     for (const mapcountry of mapcountries) {
@@ -18,20 +18,19 @@ async function render() {
     //     center: { lat: -34.397, lng: 150.644 },
     //     zoom: 8
     // });
-    // target.addEventListener('click', map());
+    target.addEventListener('click', map());
 }
 
 window.addEventListener('load', render());
 
-// async function map() {
-//     const response2 = await fetch('https://restcountries.com/v3.1/region/europe?fields=maps');
-//     const map = await response2.json();
-//     let mapimg = '';
-//     for (const country of map) {
-//         `<iframe width="600" height="450" style="border:0" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCZm8TLhPjzek0GMmnJtgZDBgyIJXG_EPE&&q=$Paris+France">
-
-//         </iframe>;`;
-//     }
-//     const target2 = document.getElementById('map');
-//     target2.innerHTML = mapimg;
-// }
+async function map() {
+    const response2 = await fetch('https://restcountries.com/v3.1/region/europe?fields=name,capital');
+    const map = await response2.json();
+    let mapimg = '';
+    for (const country of map) {
+        mapimg += `<iframe width="600" height="450" style="border:0" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCZm8TLhPjzek0GMmnJtgZDBgyIJXG_EPE&&q=${country.capital}+${country.name.official}">
+        </iframe>;`;
+    }
+    const target2 = document.getElementById('mapcountry');
+    target2.innerHTML = mapimg;
+}
