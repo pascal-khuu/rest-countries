22c0e8b36070c0f48870e148203208d5a2e887b9
Pascal Khuu <pascal.khuu@orange.fr>
2022-01-18 23:24:02 +0100

    addEventListener ('change',(event)) on selectElement

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,5 +1,5 @@
 async function render() {
-    const response = await fetch('https://restcountries.com/v3.1/region/europe?fields=name,cca2');
+    const response = await fetch('https://restcountries.com/v3.1/region/europe?fields=name,capital,cca2,latlng');
     const mapcountries = await response.json();
     let option = '';
     for (const mapcountry of mapcountries) {
@@ -8,30 +8,14 @@ async function render() {
         }
         option += `<option value="${mapcountry.cca2}"> ${mapcountry.name.official}
             </option>`;
-
-
     }
     const target = document.getElementById('mapcountries');
     target.innerHTML = option;
+    const selectElement = document.querySelector('#mapcountries');
+    selectElement.addEventListener('change', (event) => {
+        const result = document.getElementById('map');
+        result.innerHTML = `<iframe width="600" height="450" style="border:0" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCZm8TLhPjzek0GMmnJtgZDBgyIJXG_EPE&q=${event.capital[0]}+${event.name.official}&center=${event.latlng[0]},${event.latlng[1]}"></iframe>`;
+    });
 
-
-    // map.innerHTML = new google.maps.Map(document.getElementById("mapcountry"), {
-    //     center: { lat: -34.397, lng: 150.644 },
-    //     zoom: 8
-    // });
-    target.addEventListener('click', map());
 }
-
 window.addEventListener('load', render());
-
-async function map() {
-    const response2 = await fetch('https://restcountries.com/v3.1/region/europe?fields=name,capital');
-    const map = await response2.json();
-    let mapimg = '';
-    for (const country of map) {
-        mapimg += `<iframe width="600" height="450" style="border:0" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCZm8TLhPjzek0GMmnJtgZDBgyIJXG_EPE&&q=${country.capital}+${country.name.official}">
-        </iframe>;`;
-    }
-    const target2 = document.getElementById('mapcountry');
-    target2.innerHTML = mapimg;
-}
