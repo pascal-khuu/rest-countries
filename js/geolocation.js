4ba4230fe9689fd19aa97c7209a908a4606f0476
Pascal Khuu <pascal.khuu@orange.fr>
2022-01-24 16:07:08 +0100

    list of 53 countries in the dropdown list  and only one France by defaut, try to find the optimal method with src and no iframe in js to display a map of a country from the dropdown list

--- js/geolocation.js
+++ js/geolocation.js
@@ -2,23 +2,23 @@ async function render() {
     const response = await fetch('https://restcountries.com/v3.1/region/europe?fields=name,cca2,capitalInfo');
     const mapCountries = await response.json();
     let option = '';
-    let map = '';
+    let somme = 0;
     for (const mapCountry of mapCountries) {
-        if (mapCountry.cca2 === "FR") {
-            //  At the beginning, the country by defaut is France for the drop-down list
-            option = `< option  selected > ${mapCountry.name.official} < /option>`;
-            // Use a Google Embed API with a key API, it's a map centered on the capital of France (Paris) with the zoom 10 and the language which is english
-            map = `<iframe style="border:0" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/view?key=AIzaSyCZm8TLhPjzek0GMmnJtgZDBgyIJXG_EPE&center=${mapCountry.capitalInfo.latlng[0]},${mapCountry.capitalInfo.latlng[1]}&zoom=10&language=en"></iframe>`;
-        }
         // build a drop-down list of European countries 
-        option += `<option value="${mapCountry.cca2}"> ${mapCountry.name.official}</option>`;
+        if (mapCountry.cca2 == "FR") {
+            option += `<option value="${mapCountry.cca2}" selected > ${mapCountry.name.official}</option>`;
+        } else {
+            option += `<option value="${mapCountry.cca2}"> ${mapCountry.name.official}</option>`;
+        }
     }
+    console.log(somme);
+
     //a variable 'target' is a dropdow list of European countries 
     const target = document.getElementById('mapcountries');
     target.innerHTML = option;
     //a variable 'target 2' display a map centered on the capital of an European country (by defaut France and an other European country selected by the drop-down list of European countries above)
     const target2 = document.getElementById('map');
-    target2.innerHTML = map;
+    // target2.innerHTML = map;
     // an addEventListener when an user selects an European country in the drop-down list  of European countries
     target.addEventListener('change', () => displaymap(target, target2));
 }
@@ -29,7 +29,7 @@ async function displaymap(target, target2) {
     const mapDisplay = await response.json();
     let map = '';
     // Thanks to the information with the fetch, we display a map centered on the capital of an European country with the latitude and the longitude of this capital with the zoom 10 and the language which is english
-    map = `<iframe style="border:0" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/view?key=AIzaSyCZm8TLhPjzek0GMmnJtgZDBgyIJXG_EPE&center=${mapDisplay.capitalInfo.latlng[0]},${mapDisplay.capitalInfo.latlng[1]}&zoom=10&language=en"></iframe>`;
+    map = `src="https://www.google.com/maps/embed/v1/view?key=AIzaSyCZm8TLhPjzek0GMmnJtgZDBgyIJXG_EPE&center=${mapDisplay.capitalInfo.latlng[0]},${mapDisplay.capitalInfo.latlng[1]}&zoom=10&language=en"`;
     target2.innerHTML = map;
 }
 window.addEventListener('load', render());
