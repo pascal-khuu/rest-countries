2b919b9b5b4868afcdbfa833e9a109798c4bbcc2
Pascal Khuu <pascal.khuu@orange.fr>
2022-01-25 17:05:53 +0100

    Rename the variables with a meaning and optimize the selected in the drop-down list (France by default) in the file geolocation.js

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,41 +1,39 @@
 async function render() {
     // search the informations : name, code Iso 2, latitude and longitude of European countries
-    const response = await fetch('https://restcountries.com/v3.1/region/europe?fields=name,cca2,capitalInfo');
-    const mapCountries = await response.json();
+    const responseCountryEurope = await fetch('https://restcountries.com/v3.1/region/europe?fields=name,cca2');
+    const mapCountriesEurope = await responseCountryEurope.json();
     // search the informations : the latitude and the longitude of the capital of France (Paris) thanks to the code ISO fr
-    const response2 = await fetch(`https://restcountries.com/v3.1/alpha/fr?fields=capitalInfo`);
-    const france = await response2.json();
+    const responseCapitalFrance = await fetch(`https://restcountries.com/v3.1/alpha/fr?fields=capitalInfo`);
+    const infoCapitalFrance = await responseCapitalFrance.json();
     let option = '';
-    let mapfr = '';
-    for (const mapCountry of mapCountries) {
+    let mapCapitalFrance = '';
+    for (const mapCountry of mapCountriesEurope) {
         // build a drop-down list of European countries 
-        if (mapCountry.cca2 == "FR") {
-            option += `<option value="${mapCountry.cca2}" selected > ${mapCountry.name.official}</option>`;
-        } else {
-            option += `<option value="${mapCountry.cca2}"> ${mapCountry.name.official}</option>`;
-        }
+        option += `<option value="${mapCountry.cca2}"  > ${mapCountry.name.official}</option>`;
     }
-    // mapfr is an url (src) of an iframe which displays a map centered on Paris of France thanks to its latitude and its longitude with the zoom 10 and the language English.
-    mapfr = `https://www.google.com/maps/embed/v1/view?key=AIzaSyCZm8TLhPjzek0GMmnJtgZDBgyIJXG_EPE&center=${france.capitalInfo.latlng[0]},${france.capitalInfo.latlng[1]}&zoom=10&language=en`;
-    //a variable 'target' is a dropdown list of European countries 
-    const target = document.getElementById('mapcountries');
-    target.innerHTML = option;
-    //a variable 'target2' is  an iframe which displays a map centered on the capital of an European country (by defaut France and an other European country selected by the drop-down list of European countries above).
-    const target2 = document.getElementById('mapiframe');
-    // target2.src is  here an url (src) of an iframe which displays a map centered on Paris of France thanks to its latitude and its longitude with the zoom 10 and the language English.
-    target2.src = mapfr;
+    // mapCapitalFrance is an url (src) of an iframe which displays a map centered on Paris of France thanks to its latitude and its longitude with the zoom 10 and the language English.
+    mapCapitalFrance = `https://www.google.com/maps/embed/v1/view?key=AIzaSyCZm8TLhPjzek0GMmnJtgZDBgyIJXG_EPE&center=${infoCapitalFrance.capitalInfo.latlng[0]},${infoCapitalFrance.capitalInfo.latlng[1]}&zoom=10&language=en`;
+    //a variable 'selectCountry' is a dropdown list of European countries 
+    const selectCountry = document.getElementById('mapcountries');
+    selectCountry.innerHTML = option;
+    //value by default France
+    selectCountry.value = 'FR';
+    //a variable 'iframeMapCountry' is  an iframe which displays a map centered on the capital of an European country (by defaut France and an other European country selected by the drop-down list of European countries above).
+    const iframeMapCountry = document.getElementById('mapiframe');
+    // iframeMapCountry.src is  here an url (src) of an iframe which displays a map centered on Paris of France thanks to its latitude and its longitude with the zoom 10 and the language English.
+    iframeMapCountry.src = mapCapitalFrance;
     // an addEventListener when an user selects an European country in the drop-down list  of European countries.
-    target.addEventListener('change', () => displaymap(target, target2));
+    selectCountry.addEventListener('change', () => displaymap(selectCountry, iframeMapCountry));
 }
 
-async function displaymap(target, target2) {
+async function displaymap(selectCountry, iframeMapCountry) {
     //  thanks to the code ISO of a selected European country in the drop-down list above, we search  the latitude and the longitude of this country's capital.
-    const response3 = await fetch(`https://restcountries.com/v3.1/alpha/${target.value}?fields=capitalInfo`);
-    const mapDisplay = await response3.json();
-    let map = '';
+    const responseCountryEuropeCodeIso = await fetch(`https://restcountries.com/v3.1/alpha/${selectCountry.value}?fields=capitalInfo`);
+    const mapDisplay = await responseCountryEuropeCodeIso.json();
+    let mapCountryEurope = '';
     //  map is an url (src) of an iframe which displays a map centered on a capital of an European country thanks to its latitude and its longitude with the zoom 10 and the language English
-    map = `https://www.google.com/maps/embed/v1/view?key=AIzaSyCZm8TLhPjzek0GMmnJtgZDBgyIJXG_EPE&center=${mapDisplay.capitalInfo.latlng[0]},${mapDisplay.capitalInfo.latlng[1]}&zoom=10&language=en`;
-    // target2.src is  here an url (src) of an iframe which displays a map centered on on a capital of an European country  thanks to its latitude and its longitude with the zoom 10 and the language English.
-    target2.src = map;
+    mapCountryEurope = `https://www.google.com/maps/embed/v1/view?key=AIzaSyCZm8TLhPjzek0GMmnJtgZDBgyIJXG_EPE&center=${mapDisplay.capitalInfo.latlng[0]},${mapDisplay.capitalInfo.latlng[1]}&zoom=10&language=en`;
+    // iframeMapCountry.src is  here an url (src) of an iframe which displays a map centered on on a capital of an European country  thanks to its latitude and its longitude with the zoom 10 and the language English.
+    iframeMapCountry.src = mapCountryEurope;
 }
 window.addEventListener('load', render());
